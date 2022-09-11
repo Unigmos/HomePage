fa8b3235ab63b1564adbafcd7bcceff6467f7a88
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-09-12 08:46:18 +0900

    Add files via upload

---
+++ parts/profile.php
@@ -0,0 +1,17 @@
+<div class="admin_info">
+    <div class="admin_top">
+        <div class="admin_image">
+            <img src="https://pbs.twimg.com/profile_images/1363728876107522053/bORtDFkE_400x400.jpg">
+        </div>
+        <div class="admin_name">
+            <p>Shaneron</p>
+        </div>
+    </div>
+    <p class="admin_description">
+        このサイトの作成者兼管理人です。<br>
+        主にHTMLやPythonをいじっています！
+    </p>
+    <a class="portfolio_button" href="http://localhost/wordpress/wp-content/themes/mysite/portfolio.php">
+        <p>ポートフォリオ</p>
+    </a>
+</div>
