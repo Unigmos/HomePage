1c39eca2b991fd05a30812b0d6e2a11bd95c3077
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-09-11 20:37:26 +0900

    Add files via upload

---
+++ portfolio/portfolio.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Shaneron">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shaneron.page</title>
+    <link rel="stylesheet" href="http://localhost/wordpress/wp-content/themes/mysite/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:wght@500&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+
+
+<body>
+    <header>
+        <div class="header_menu">
+            <nav>
+                <ul>
+                    <li><a href="#top">Top</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#works">Works</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="top_container" id="top">
+            <div class="top_title">
+                <h2>Welcome to Shaneron's portfolio</h2>
+            </div>
+            <div class="top_image">
+                <img src="https://pbs.twimg.com/profile_images/1363728876107522053/bORtDFkE_400x400.jpg">
+            </div>
+            <div class="top_message">
+                <p>
+                    こちらはShaneronのポートフォリオになります。<br>
+                    作成したサイトやサービスを掲載しています。
+                </p>
+            </div>
+        </div>
+        <div class="about_container">
+            <div class="about_message">
+                <h2 id="about">About</h2>
+                <p>
+                    &emsp;しゃねろんと言います。普段はお酒片手にYoutubeを見るような大学生ですが、やるときはやるかもしれない男です。<br>
+                    &emsp;言語は主にPythonとHTMLを使用していて、最近はスマホアプリ開発関連でKotlinにも手を出し始めました。<br>
+                    &emsp;良いお酒とライブラリを模索していますー。気軽に接して頂けると嬉しいです、よろしくお願いしますー。
+                </p>
+            </div>
+        </div>
+
+        <div class="service_container">
+            <div class="service_message">
+                <h2 id="works">Works</h2>
+            </div>
+            <div class="service_box">
+                <div class="service_data slide_in">
+                    <a href="#">
+                        <div class="img_box">
+                            <img src="https://placehold.jp/1200x630.png"/>
+                        </div>
+                    </a>
+                    <div class="service_content">
+                        <h3>&nbsp;Shaneron.page</h3>
+                    </div>
+                    <div class="link_box">
+                        <a class="link_button under_line" href="#">Site</a>
+                        <a class="link_button under_line" href="https://github.com/Unigmos/HomePage">GitHub</a>
+                    </div>
+                </div>
+
+                <div class="service_data slide_in">
+                    <a href="#">
+                        <div class="img_box">
+                            <img src="https://placehold.jp/1200x630.png"/>
+                        </div>
+                    </a>
+                    <div class="service_content">
+                        <h3>&nbsp;TitleText</h3>
+                    </div>
+                    <div class="link_box">
+                        <a class="link_button under_line" href="#">Site</a>
+                    </div>
+                </div>
+
+                <div class="service_data slide_in">
+                    <a href="#">
+                        <div class="img_box">
+                            <img src="https://placehold.jp/1200x630.png"/>
+                        </div>
+                    </a>
+                    <div class="service_content">
+                        <h3>&nbsp;TitleText</h3>
+                    </div>
+                    <div class="link_box">
+                        <a class="link_button under_line" href="#">Site</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="contact_container">
+            <div class="contact_message">
+                <h2 id="contact">Contact</h2>
+                <ul>
+                    <li><a href="https://twitter.com/intent/follow?screen_name=Shaneron_kijo"><b>Twitter:</b>@shaneron_kijo</a></li>
+                    <li><a href="mailto:shaneron@sumahotektek.com"><b>Gmail:</b>shaneron@sumahotektek.com</a></li>
+					<li><a href="https://github.com/Unigmos"><b>GitHub:</b>https://github.com/Unigmos</a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
