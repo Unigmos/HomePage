49272c788ae7d9731b6e6587898bbb92a6363408
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-09-20 15:15:11 +0900

    Update footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -1,4 +1,4 @@
 <div class="footer_content">
     <a href="#">利用規約</a>
-    <p>Copyright © 2022-{yyyy} ○○○○ All Rights Reserved.</p>
+    <p>Copyright © 2022-<?php echo date('Y') ?> Shaneron All Rights Reserved.</p>
 </div>
