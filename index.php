371a7ecd0f6a78fa7ab52818e0d40f04c7bf6554
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-09-20 15:15:56 +0900

    footer.php呼び出し

--- index.php
+++ index.php
@@ -83,10 +83,7 @@
 	</main>
 	<footer>
 		<?wp_footer();?>
-        <div class="footer_content">
-            <a href="#">利用規約</a>
-            <p>Copyright © 2022-{yyyy} ○○○○ All Rights Reserved.</p>
-        </div>
+        	<?php get_footer(); ?>
 	</footer>
 </body>
 </html>
