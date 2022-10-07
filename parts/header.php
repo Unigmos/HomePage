4975552935bd74225f3e0dc8454378f1d75ab7e8
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-10-07 13:14:41 +0900

    Add files via upload

---
+++ parts/header.php
@@ -0,0 +1,7 @@
+<div class="header_menu">
+	<a href="https://shaneron.com">
+		<h1>
+			<img src="https://shaneron.com/wp-content/themes/shaneron/image/site_logo_300x300.png" alt="Shaneron.comのロゴ" width="30px" height="30px">
+		</h1>
+	</a>
+</div>
