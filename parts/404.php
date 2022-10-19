4772b6b19e4e82a992d11c42eba7c892f3153de1
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-10-19 17:59:08 +0900

    404ページ

---
+++ parts/404.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--
+    Welcome to 404!!
+	何か間違えましたね？
+-->
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Shaneron">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shaneron.page</title>
+	<link rel="icon" href="https://shaneron.com/wp-content/themes/shaneron/favicon/page_logo_32x32.ico">
+    <link rel="stylesheet" href="https://shaneron.com/wp-content/themes/shaneron/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+	<?if(is_singular()){?>
+		<style>
+			h2{
+			  padding: 0.25em;
+			  color: #010101;
+			  background: #ffe18f;
+			  border-bottom: solid 3px #f6ba33;
+			}
+			h3{
+			  color: #505050;
+			  padding: 0.5em;
+			  display: inline-block;
+			  line-height: 1.3;
+			  background: #ffe18f;
+			  vertical-align: middle;
+			  border-radius: 25px 0px 0px 25px;
+			}
+			h3:before {
+			  content: '●';
+			  color: white;
+			  margin-right: 8px;
+			}
+			h4{
+				border-bottom: solid 2px #ffe18f;
+			}
+			h4:before{
+				font-family: "Font Awesome 6 Free";
+				content: '\f00c';
+				font-weight: 900;
+				color: #ffe18f;
+				margin-right: 8px;
+			}
+			table{
+				width: 100%;
+				border-collapse: collapse;
+				border-spacing: 0;
+				border: 1px solid #eee;
+			}
+			table th,table td{
+			  	padding: 10px 0;
+			  	text-align: center;
+			}
+
+			table tr:nth-child(odd){
+			 	background-color: #eee;
+			}
+			td{
+				border: 1px solid #999;
+				font-weight:600;
+			}
+		</style>
+	<?}?>
+    <?wp_head();?>
+</head>
+<body>
+	<header>
+		<?php get_header(); ?>
+	</header>
+	<main class="single_main">
+        <div class="article_box">
+            <div class="article_title">
+                <h1>404</h1>
+            </div>
+			
+            <div class="article_image">
+				<img src="https://blog.sumahotektek.com/wp-content/uploads/2021/02/no-image.jpg" alt="404">
+            </div>
+            <div class="article_sentence">
+				<p>ページが見つかりませんでした</p>
+            </div>
+        </div>
+        
+        <div class="side_container">
+            <?php get_template_part('profile'); ?>
+        </div>
+
+    </main>
+	<footer>
+		<?wp_footer();?>
+        <?php get_footer(); ?>
+	</footer>
+</body>
+</html>
